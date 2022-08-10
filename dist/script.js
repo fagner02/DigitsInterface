1a99b9f87ab8aae851f65601ca326d3af16e2002
Fagner <fagnerrc12@gmail.com>
2022-08-10 09:13:41 -0300

    Add animations and AI

--- dist/script.js
+++ dist/script.js
@@ -36,6 +36,8 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 var size = 10;
 var blocksNum = 784;
+var num = 0;
+var angle = 0;
 var activations = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -77,22 +79,117 @@ var Block = /** @class */ (function () {
         this.instantiate();
     }
     Block.prototype.setColor = function () {
-        this.block.style.backgroundColor =
+        // this.timeout = setTimeout(() => {
+        this.block.children[this.behind].style.backgroundColor =
             "hsl(0, 0%, " +
-                (90.0 - Math.round(activations[this.index] + 1) / 2.83) +
+                (90 - Math.round((activations[this.index] + 1) / 2.83)) +
                 "%)";
+        this.block.style.transform = "rotateY(" + angle + "deg)";
+        // clearTimeout(this.timeout);
+        // }, 10 * this.index);
+        this.behind = this.behind == 0 ? 1 : 0;
     };
     Block.prototype.instantiate = function () {
         this.block = document.createElement("div");
         this.block.className = "block";
         this.block.style.width = size + "px";
         this.block.style.height = size + "px";
-        this.setColor();
-        this.block.style.transform = "perspective(100px) rotateY(0deg)";
+        var front = document.createElement("div");
+        front.className = "block";
+        front.style.backgroundColor = "hsl(0, 0%, 90%)";
+        front.style.width = size + "px";
+        front.style.height = size + "px";
+        front.style.position = "absolute";
+        front.style.transform = "rotateY(180deg)";
+        var back = document.createElement("div");
+        back.className = "block";
+        back.style.backgroundColor = "hsl(0, 0%, 0%)";
+        back.style.width = size + "px";
+        back.style.height = size + "px";
+        back.style.position = "absolute";
+        // this.block.style.transform = "rotateY(0deg)";
+        var delay = Math.round(this.index / 20) * 150;
+        this.block.style.transition = "transform 2s " + delay + "ms";
+        console.log(this.block.style.transition);
         document.querySelector(".box").appendChild(this.block);
+        this.behind = 0;
+        this.block.appendChild(front);
+        this.block.appendChild(back);
     };
     return Block;
 }());
+var weights = [];
+var biases = [];
+function sigmoid(layer) {
+    var newLayer = [];
+    newLayer.fill(0.0, 0, layer.length - 1);
+    for (var i = 0; i < layer.length; i++) {
+        newLayer[i] = 1.0 / (1.0 + Math.exp(-layer[i]));
+    }
+    return newLayer;
+}
+function feedForward(layer) {
+    for (var i = 0; i < weights.length; i++) {
+        var newLayer = [];
+        for (var j = 0; j < weights[i].length; j++) {
+            var sigmoidOut = 0.0;
+            for (var k = 0; k < weights[i][j].length; k++) {
+                sigmoidOut += weights[i][j][k] * layer[k];
+            }
+            newLayer.push(sigmoidOut / 10.0 + biases[i][j]);
+        }
+        layer = sigmoid(newLayer);
+    }
+    return layer;
+}
+function evaluateInput() {
+    activations = activations.map(function (value) { return value / 255.0; });
+    var res = feedForward(activations);
+    num = res.indexOf(Math.max.apply(Math, res));
+    console.log(res);
+    var img = new Image(28, 28);
+    img.src = num + ".png";
+    img.onload = function () {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        var context = canvas.getContext("2d");
+        context.drawImage(img, 0, 0);
+        var data = context.getImageData(0, 0, img.width, img.height).data;
+        angle = angle == 0 ? 180 : 0;
+        for (var i = 0; i < data.length; i += 4) {
+            activations[Math.min(i / 4)] = 255 - data[i];
+            blocks[Math.min(i / 4)].setColor();
+        }
+    };
+}
+function readText(text) {
+    var inputs = text.split("\n");
+    var sizes = [784, 50, 50, 10];
+    for (var i = 0; i < sizes.length - 1; i++) {
+        var result = [];
+        biases.push([]);
+        for (var j = 0; j < sizes[i + 1]; j++) {
+            var temp = void 0;
+            result.push([]);
+            for (var k = 0; k < sizes[i]; k++) {
+                temp = parseFloat(inputs[0]);
+                inputs.shift();
+                result[j].push(temp);
+            }
+            temp = parseFloat(inputs[0]);
+            inputs.shift();
+            biases[i].push(temp);
+        }
+        weights.push(result);
+    }
+}
+function readFile() {
+    console.log("Reading values");
+    fetch("save.txt")
+        .then(function (response) { return response.text(); })
+        .then(function (text) { return readText(text); });
+}
 document.querySelector(".box").style.gap = 2 + "px";
 var gapSize = parseInt(document.querySelector(".box").style.gap);
 console.log(gapSize);
@@ -105,49 +202,25 @@ function sleep(ms) {
 }
 function start() {
     return __awaiter(this, void 0, void 0, function () {
-        var img, i;
-        var _this = this;
+        var i, i;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    img = new Image(28, 28);
-                    img.src = "0.png";
-                    img.onload = function () {
-                        var canvas = document.createElement("canvas");
-                        canvas.width = img.width;
-                        canvas.height = img.height;
-                        var context = canvas.getContext("2d");
-                        context.drawImage(img, 0, 0);
-                        var data = context.getImageData(0, 0, img.width, img.height).data;
-                        for (var i = 0; i < data.length; i += 4) {
-                            console.log(data[i]);
-                            activations[Math.min(i / 4)] = 255 - data[i];
-                            blocks[Math.min(i / 4)].setColor();
-                        }
-                    };
+                    readFile();
                     for (i = 0; i < blocksNum; i++) {
                         blocks.push(new Block(i));
                     }
-                    return [4 /*yield*/, sleep(1000)];
+                    return [4 /*yield*/, sleep(5000)];
                 case 1:
                     _a.sent();
-                    blocks.forEach(function (b) { return __awaiter(_this, void 0, void 0, function () {
-                        var angle;
-                        return __generator(this, function (_a) {
-                            switch (_a.label) {
-                                case 0: return [4 /*yield*/, sleep(100)];
-                                case 1:
-                                    _a.sent();
-                                    angle = parseInt(b.block.style.transform.split(" ")[1].replace("rotateY(", ""));
-                                    b.block.style.transform =
-                                        "perspective(100px) rotateY(" + ((angle + 180) % 261) + "deg)";
-                                    return [2 /*return*/];
-                            }
-                        });
-                    }); });
+                    angle = angle == 0 ? 180 : 0;
+                    for (i = 0; i < blocks.length; i++) {
+                        blocks[i].setColor();
+                    }
                     return [2 /*return*/];
             }
         });
     });
 }
 start();
+//# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,7 @@
 const size = 10;
 const blocksNum = 784;
+let num = 0;
+let angle = 0;
 let activations: number[] = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -39,25 +41,141 @@ let activations: number[] = [
 class Block {
   block: HTMLElement;
   index: number;
+  timeout: number;
+  behind: number;
   constructor(index: number) {
     this.index = index;
     this.instantiate();
   }
   setColor() {
-    this.block.style.backgroundColor =
+    // this.timeout = setTimeout(() => {
+    (<HTMLElement>this.block.children[this.behind]).style.backgroundColor =
       "hsl(0, 0%, " +
-      (90.0 - Math.round(activations[this.index] + 1) / 2.83) +
+      (90 - Math.round((activations[this.index] + 1) / 2.83)) +
       "%)";
+    this.block.style.transform = "rotateY(" + angle + "deg)";
+    // clearTimeout(this.timeout);
+    // }, 10 * this.index);
+    this.behind = this.behind == 0 ? 1 : 0;
   }
   instantiate() {
     this.block = document.createElement("div");
     this.block.className = "block";
     this.block.style.width = size + "px";
     this.block.style.height = size + "px";
-    this.setColor();
-    this.block.style.transform = "perspective(100px) rotateY(0deg)";
+    let front = document.createElement("div");
+    front.className = "block";
+    front.style.backgroundColor = "hsl(0, 0%, 90%)";
+    front.style.width = size + "px";
+    front.style.height = size + "px";
+    front.style.position = "absolute";
+    front.style.transform = "rotateY(180deg)";
+    let back = document.createElement("div");
+    back.className = "block";
+    back.style.backgroundColor = "hsl(0, 0%, 0%)";
+    back.style.width = size + "px";
+    back.style.height = size + "px";
+    back.style.position = "absolute";
+    // this.block.style.transform = "rotateY(0deg)";
+    let delay = Math.round(this.index / 20) * 150;
+    this.block.style.transition = "transform 2s " + delay + "ms";
+    console.log(this.block.style.transition);
     (<Element>document.querySelector(".box")).appendChild(this.block);
+    this.behind = 0;
+    this.block.appendChild(front);
+    this.block.appendChild(back);
   }
+}
+
+let weights: number[][][] = [];
+let biases: number[][] = [];
+
+function sigmoid(layer: number[]): number[] {
+  let newLayer: number[] = [];
+  newLayer.fill(0.0, 0, layer.length - 1);
+
+  for (let i = 0; i < layer.length; i++) {
+    newLayer[i] = 1.0 / (1.0 + Math.exp(-layer[i]));
+  }
+
+  return newLayer;
+}
+
+function feedForward(layer: number[]) {
+  for (let i = 0; i < weights.length; i++) {
+    var newLayer: number[] = [];
+
+    for (let j = 0; j < weights[i].length; j++) {
+      let sigmoidOut = 0.0;
+
+      for (let k = 0; k < weights[i][j].length; k++) {
+        sigmoidOut += weights[i][j][k] * layer[k];
+      }
+
+      newLayer.push(sigmoidOut / 10.0 + biases[i][j]);
+    }
+
+    layer = sigmoid(newLayer);
+  }
+
+  return layer;
+}
+
+function evaluateInput() {
+  activations = activations.map((value) => value / 255.0);
+
+  let res = feedForward(activations);
+  num = res.indexOf(Math.max(...res));
+  console.log(res);
+
+  let img = new Image(28, 28);
+  img.src = num + ".png";
+  img.onload = () => {
+    let canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    let context = canvas.getContext("2d");
+    context.drawImage(img, 0, 0);
+    let data = context.getImageData(0, 0, img.width, img.height).data;
+
+    angle = angle == 0 ? 180 : 0;
+    for (let i = 0; i < data.length; i += 4) {
+      activations[Math.min(i / 4)] = 255 - data[i];
+      blocks[Math.min(i / 4)].setColor();
+    }
+  };
+}
+
+function readText(text: string) {
+  let inputs = text.split("\n");
+  const sizes = [784, 50, 50, 10];
+
+  for (let i = 0; i < sizes.length - 1; i++) {
+    let result: number[][] = [];
+    biases.push([]);
+    for (let j = 0; j < sizes[i + 1]; j++) {
+      let temp: number;
+      result.push([]);
+
+      for (let k = 0; k < sizes[i]; k++) {
+        temp = parseFloat(inputs[0]);
+        inputs.shift();
+        result[j].push(temp);
+      }
+
+      temp = parseFloat(inputs[0]);
+      inputs.shift();
+      biases[i].push(temp);
+    }
+    weights.push(result);
+  }
+}
+
+function readFile() {
+  console.log("Reading values");
+  fetch("save.txt")
+    .then((response) => response.text())
+    .then((text) => readText(text));
 }
 
 (<HTMLElement>document.querySelector(".box")).style.gap = 2 + "px";
@@ -77,36 +195,15 @@ function sleep(ms: number) {
 }
 
 async function start() {
-  let img = new Image(28, 28);
-  img.src = "0.png";
-  img.onload = () => {
-    let canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    let context = canvas.getContext("2d");
-    context.drawImage(img, 0, 0);
-    let data = context.getImageData(0, 0, img.width, img.height).data;
-
-    for (let i = 0; i < data.length; i += 4) {
-      console.log(data[i]);
-      activations[Math.min(i / 4)] = 255 - data[i];
-      blocks[Math.min(i / 4)].setColor();
-    }
-  };
-
+  readFile();
   for (let i = 0; i < blocksNum; i++) {
     blocks.push(new Block(i));
   }
-
-  await sleep(1000);
-  blocks.forEach(async (b) => {
-    await sleep(100);
-    var angle = parseInt(
-      b.block.style.transform.split(" ")[1].replace("rotateY(", "")
-    );
-    b.block.style.transform =
-      "perspective(100px) rotateY(" + ((angle + 180) % 261) + "deg)";
-  });
+  await sleep(5000);
+  angle = angle == 0 ? 180 : 0;
+  for (let i = 0; i < blocks.length; i++) {
+    blocks[i].setColor();
+  }
 }
 
 start();
